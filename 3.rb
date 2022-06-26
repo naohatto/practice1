5c71398e26ba143469bc605db9ee74015791b9dd
naohatto <naohatto113@gmail.com>
2022-06-26 01:35:35 +0000

    Initial Commit

---
+++ 3.rb
@@ -0,0 +1,8 @@
+total = 0
+num = 0
+while total < 9
+  num += 1
+  total += (1.0 / (num))
+end
+puts total
+puts num
---
+++ 4.rb
@@ -0,0 +1,13 @@
+num = 0
+total = 0
+while (num <= 50000) do
+
+  if (num % 3 == 0 || num.to_s.include?("3"))
+    #3の倍数とは、数字を3で割りき切れる数字なので「%3」
+    #3のつく数字はnumの数を文字列に変換し、「to_s」、
+    #指定した文字列"3"が含まれている場合にtrueを返す条件を加える。「.include?("3")」
+    total += num
+  end
+  num = num + 1
+end
+puts total
